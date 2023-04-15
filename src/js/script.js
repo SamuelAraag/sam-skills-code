51716f144a52cc48604f1af01d1f17d8e7f7fe6b
Samuel Santos <samuelsantos@MacBook-Air-de-Samuel.local>
2023-04-15 17:53:07 -0300

    criado cabeçalho e efeito blur com opções e efeito parallax

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,9 @@ var inicioPaddin = 26;
 window.addEventListener('scroll', () => {
     var fundo0 = document.getElementById('fundo0');
     var fundo1 = document.getElementById('fundo1');
-    var fundo2 = document.getElementById('fundo2');
 
     let scroll = window.scrollY;
 
     fundo0.style.top = scroll * 0.05 + '%'
     fundo1.style.top = scroll * 0.03 + '%';
-    // fundo2.style.marginTop = inicioPaddin - (scroll * 0.1) + '%';
-
 })
