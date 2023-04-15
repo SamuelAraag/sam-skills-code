35d141d3e30813248fda0590944097f8b40e59f0
Samuel Santos <samuelsantos@MacBook-Air-de-Samuel.local>
2023-04-15 13:24:27 -0300

    corrigido movimento do efeito parallax

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+var inicioPaddin = 26;
 window.addEventListener('scroll', () => {
     var fundo0 = document.getElementById('fundo0');
     var fundo1 = document.getElementById('fundo1');
@@ -5,8 +6,8 @@ window.addEventListener('scroll', () => {
 
     let scroll = window.scrollY;
 
-    fundo1.style.marginTop = scroll * -0.03 + '%'
+    fundo0.style.top = scroll * 0.05 + '%'
+    fundo1.style.top = scroll * 0.03 + '%';
+    // fundo2.style.marginTop = inicioPaddin - (scroll * 0.1) + '%';
 
-    debugger
-    fundo2.style.marginTop = scroll * -0.05 + '%'
 })
